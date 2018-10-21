9558a432762896983bb8302d010ce2021f01f890
zwc0 <19657555+zwc0@users.noreply.github.com>
2018-10-21 16:44:46 -0600

    fixed

--- modmain.lua
+++ modmain.lua
@@ -17,96 +17,136 @@ PrefabFiles = {
 Assets = {
     Asset( "IMAGE", "images/saveslot_portraits/alex.tex" ),
     Asset( "ATLAS", "images/saveslot_portraits/alex.xml" ),
+
     Asset( "IMAGE", "images/selectscreen_portraits/alex.tex" ),
-    Asset( "ATLAS", "images/selectscreen_portraits/alex.xml" ),	
+    Asset( "ATLAS", "images/selectscreen_portraits/alex.xml" ),
+	
     Asset( "IMAGE", "images/selectscreen_portraits/alex_silho.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/alex_silho.xml" ),
+
     Asset( "IMAGE", "bigportraits/alex.tex" ),
     Asset( "ATLAS", "bigportraits/alex.xml" ),
+	
 	Asset( "IMAGE", "images/map_icons/alex.tex" ),
 	Asset( "ATLAS", "images/map_icons/alex.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/avatar_alex.tex" ),
     Asset( "ATLAS", "images/avatars/avatar_alex.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/self_inspect_alex.tex" ),
     Asset( "ATLAS", "images/avatars/self_inspect_alex.xml" ),
+	
 	Asset( "IMAGE", "images/names_alex.tex" ),
     Asset( "ATLAS", "images/names_alex.xml" ),
+	
     Asset( "IMAGE", "bigportraits/alex_none.tex" ),
     Asset( "ATLAS", "bigportraits/alex_none.xml" ),
 	
 	Asset( "IMAGE", "images/saveslot_portraits/austin.tex" ),
     Asset( "ATLAS", "images/saveslot_portraits/austin.xml" ),
+
     Asset( "IMAGE", "images/selectscreen_portraits/austin.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/austin.xml" ),
+	
     Asset( "IMAGE", "images/selectscreen_portraits/austin_silho.tex" ),
-    Asset( "ATLAS", "images/selectscreen_portraits/austin_silho.xml" )
+    Asset( "ATLAS", "images/selectscreen_portraits/austin_silho.xml" ),
+
     Asset( "IMAGE", "bigportraits/austin.tex" ),
     Asset( "ATLAS", "bigportraits/austin.xml" ),
+	
 	Asset( "IMAGE", "images/map_icons/austin.tex" ),
 	Asset( "ATLAS", "images/map_icons/austin.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/avatar_austin.tex" ),
     Asset( "ATLAS", "images/avatars/avatar_austin.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/self_inspect_austin.tex" ),
     Asset( "ATLAS", "images/avatars/self_inspect_austin.xml" ),
+	
 	Asset( "IMAGE", "images/names_austin.tex" ),
     Asset( "ATLAS", "images/names_austin.xml" ),
+	
     Asset( "IMAGE", "bigportraits/austin_none.tex" ),
     Asset( "ATLAS", "bigportraits/austin_none.xml" ),
 
     Asset( "IMAGE", "images/saveslot_portraits/gracie.tex" ),
     Asset( "ATLAS", "images/saveslot_portraits/gracie.xml" ),
+
     Asset( "IMAGE", "images/selectscreen_portraits/gracie.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/gracie.xml" ),
+	
     Asset( "IMAGE", "images/selectscreen_portraits/gracie_silho.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/gracie_silho.xml" ),
+
     Asset( "IMAGE", "bigportraits/gracie.tex" ),
     Asset( "ATLAS", "bigportraits/gracie.xml" ),
+	
 	Asset( "IMAGE", "images/map_icons/gracie.tex" ),
 	Asset( "ATLAS", "images/map_icons/gracie.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/avatar_gracie.tex" ),
     Asset( "ATLAS", "images/avatars/avatar_gracie.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/self_inspect_gracie.tex" ),
     Asset( "ATLAS", "images/avatars/self_inspect_gracie.xml" ),
+	
 	Asset( "IMAGE", "images/names_gracie.tex" ),
     Asset( "ATLAS", "images/names_gracie.xml" ),
+	
     Asset( "IMAGE", "bigportraits/gracie_none.tex" ),
     Asset( "ATLAS", "bigportraits/gracie_none.xml" ),
 	
 	Asset( "IMAGE", "images/saveslot_portraits/kaylee.tex" ),
     Asset( "ATLAS", "images/saveslot_portraits/kaylee.xml" ),
+
     Asset( "IMAGE", "images/selectscreen_portraits/kaylee.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/kaylee.xml" ),
+	
     Asset( "IMAGE", "images/selectscreen_portraits/kaylee_silho.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/kaylee_silho.xml" ),
+
     Asset( "IMAGE", "bigportraits/kaylee.tex" ),
     Asset( "ATLAS", "bigportraits/kaylee.xml" ),
+	
 	Asset( "IMAGE", "images/map_icons/kaylee.tex" ),
 	Asset( "ATLAS", "images/map_icons/kaylee.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/avatar_kaylee.tex" ),
     Asset( "ATLAS", "images/avatars/avatar_kaylee.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/self_inspect_kaylee.tex" ),
     Asset( "ATLAS", "images/avatars/self_inspect_kaylee.xml" ),
+	
 	Asset( "IMAGE", "images/names_kaylee.tex" ),
     Asset( "ATLAS", "images/names_kaylee.xml" ),
+	
     Asset( "IMAGE", "bigportraits/kaylee_none.tex" ),
     Asset( "ATLAS", "bigportraits/kaylee_none.xml" ),
 	
 	Asset( "IMAGE", "images/saveslot_portraits/matt.tex" ),
     Asset( "ATLAS", "images/saveslot_portraits/matt.xml" ),
+
     Asset( "IMAGE", "images/selectscreen_portraits/matt.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/matt.xml" ),
+	
     Asset( "IMAGE", "images/selectscreen_portraits/matt_silho.tex" ),
     Asset( "ATLAS", "images/selectscreen_portraits/matt_silho.xml" ),
+
     Asset( "IMAGE", "bigportraits/matt.tex" ),
     Asset( "ATLAS", "bigportraits/matt.xml" ),
+	
 	Asset( "IMAGE", "images/map_icons/matt.tex" ),
 	Asset( "ATLAS", "images/map_icons/matt.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/avatar_matt.tex" ),
     Asset( "ATLAS", "images/avatars/avatar_matt.xml" ),
+	
 	Asset( "IMAGE", "images/avatars/self_inspect_matt.tex" ),
     Asset( "ATLAS", "images/avatars/self_inspect_matt.xml" ),
+	
 	Asset( "IMAGE", "images/names_matt.tex" ),
     Asset( "ATLAS", "images/names_matt.xml" ),
+	
     Asset( "IMAGE", "bigportraits/matt_none.tex" ),
     Asset( "ATLAS", "bigportraits/matt_none.xml" ),
 }
@@ -118,7 +158,7 @@ GLOBAL.ATT = GetModConfigData("STATS")
 GLOBAL.MODE = GetModConfigData("MODE")
 
 GLOBAL.UPGRADE = function(inst)
-	local max_upgrades = 10000
+	local max_upgrades = 50
 	local upgrades = math.min(inst.level, max_upgrades)
 	local increase = 5
 	local hunger_percent = inst.components.hunger:GetPercent()
@@ -176,9 +216,10 @@ GLOBAL.ATF = function(inst,nm)
 	inst.components.health:SetMaxHealth(GLOBAL.ATT)
 	inst.components.hunger:SetMax(GLOBAL.ATT)
 	inst.components.sanity:SetMax(GLOBAL.ATT)
-	inst.components.health:StartRegen(2 , 1, false)
-	inst.components.sanity.night_drain_mult = -2
-	inst.components.combat.damagemultiplier = 1
+	inst.components.health:StartRegen(1 , 1, false)
+	--inst.components.health:StartRegen(2 , 1, false) --2/s
+	--inst.components.sanity.night_drain_mult = -2
+	--inst.components.combat.damagemultiplier = 1
 	inst.components.hunger.hungerrate = 0.1 * TUNING.WILSON_HUNGER_RATE
 	inst.components.locomotor.walkspeed = 15
 	inst.components.locomotor.runspeed = 15
